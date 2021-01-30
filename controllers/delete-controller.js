1be6da5644c286c12a80b24b7b68dd9287323a66
Bartłomiej Drążkiewicz <drazkiewiczbart@icloud.com>
2021-01-30 11:09:56 +0100

    Refactor delete controller

--- controllers/delete-controller.js
+++ controllers/delete-controller.js
@@ -4,15 +4,15 @@ const mongoose = require('mongoose');
 const user = mongoose.model('users');
 
 const deleteUser = (req, res) => {
-  user.deleteOne({ _id: req.user }, (error, object) => {
-    if(error) {
-      req.flash('error', 'Sorry, we can\'t delete your account. Please try again later');
-      res.redirect('/profile');
-    } else {
-      req.logout();
-      req.flash('success', 'Your account was deleted successful');
-      res.redirect('/');
-    };
+  user.deleteOne({ _id: req.user }).exec()
+  .then(_ => {
+    req.logout();
+    req.flash('success', 'Your account was deleted successful');
+    res.redirect('/');
+  })
+  .catch(_ => {
+    req.flash('err', 'Sorry, we can\'t delete your account. Please try again later');
+    res.redirect('/profile');
   });
 };
 
